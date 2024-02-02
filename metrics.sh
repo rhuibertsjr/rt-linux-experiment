8ad68acf0e7dece9493a2c825e9cc8383f08c227
rhuibertsjr <rhuibertsjr@gmail.com>
2024-02-02 08:52:27 +0000

    Fixed: Real-time cyclic metrics

--- metrics.sh
+++ metrics.sh
@@ -2,5 +2,5 @@
 
 sudo cyclictest -l10000000 -m -S -p90 -i200 -h400 -q > output.txt
 
-grep -v -e "^#" -e "^$" output.txt | tr " " "," | tr "\t" "," > histogram.csv
-sed -i '1s/^/time,core1,core2,core3,core4\n /' histogram.csv
+grep -v -e "^#" -e "^$" output.txt | tr " " "," | tr "\t" "," > histogram-rt.csv
+sed -i '1s/^/time,core1,core2,core3,core4\n /' histogram-rt.csv
